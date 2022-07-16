85367458a797dd7f0e1abbe7acc9172a53aafe58
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-16 18:42:37 +0300

    test smart table

--- cypress/e2e/testWithPageObjects.cy.js
+++ cypress/e2e/testWithPageObjects.cy.js
@@ -1,6 +1,7 @@
 import { onDatePickerPage } from "../support/page_objects/datepickerPage";
 import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage";
 import { navigateTo } from "../support/page_objects/navigationPage";
+import { onSmartTable } from "../support/page_objects/smartTablePage";
 
 describe("Test with Page Objects", () => {
   beforeEach("Open application", () => {
@@ -28,5 +29,9 @@ describe("Test with Page Objects", () => {
     navigateTo.datePickerPage();
     onDatePickerPage.selectCommonDatepickerDateFromToday(1);
     onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14);
+    navigateTo.smartTablePage();
+    onSmartTable.addNewRecordWithFirstAndLastName("Artem", "Bardem");
+    onSmartTable.updateAgeByFirstName("Artem", 20);
+    onSmartTable.deleteRowByIndex(1);
   });
 });
