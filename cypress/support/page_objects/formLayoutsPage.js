d249eb85da6b25bbed88eb74d31f427b39b895cb
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-16 17:57:59 +0300

    submit basic form with email and password

--- cypress/support/page_objects/formLayoutsPage.js
+++ cypress/support/page_objects/formLayoutsPage.js
@@ -9,6 +9,18 @@ export class FormLayoutsPage {
         cy.wrap(form).submit();
       });
   }
+
+  submitBasicFormWithEmailAndPassword(email, password) {
+    cy.contains("nb-card", "Basic form")
+      .find("form")
+      .then((form) => {
+        cy.wrap(form).find('[placeholder="Email"]').type(email);
+        cy.wrap(form).find('[placeholder="Password"]').type(password);
+        cy.wrap(form).find('[type="checkbox"]').check({ force: true });
+
+        cy.wrap(form).submit();
+      });
+  }
 }
 
 export const onFormLayoutsPage = new FormLayoutsPage();
